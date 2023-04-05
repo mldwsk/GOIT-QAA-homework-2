ff6db518eaa977ac1fb09749016e425c70785a3d
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-04-05 20:27:16 +0200

    Optimized structure

---
+++ cypress/e2e/tasks1and2.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("GOIT homework 2, task 1", () => {
+  beforeEach(() => {
+    cy.visit("https://www.edu.goit.global/account/login");
+  })
+  it('logs in and out 1', () => {
+    cy.login('user888@gmail.com', '1234567890');
+    cy.get('#open-navigation-menu-mobile').click();
+    cy.contains('button', 'Log out').click();
+  })
+  it('logs in and out 2', () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.login('testowyqa@qa.team', 'QA!automation-1');
+    cy.get('#open-navigation-menu-mobile').click();
+    cy.contains('button', 'Log out').click();
+  })
+});
