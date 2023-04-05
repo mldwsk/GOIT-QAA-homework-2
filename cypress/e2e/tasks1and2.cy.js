5b0c6409eb92c5784a27b3cefbcebfb52b209e50
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-04-05 20:29:30 +0200

    Fixed test 2

--- cypress/e2e/tasks1and2.cy.js
+++ cypress/e2e/tasks1and2.cy.js
@@ -10,7 +10,6 @@ describe("GOIT homework 2, task 1", () => {
     cy.contains('button', 'Log out').click();
   })
   it('logs in and out 2', () => {
-    cy.visit("https://www.edu.goit.global/account/login");
     cy.login('testowyqa@qa.team', 'QA!automation-1');
     cy.get('#open-navigation-menu-mobile').click();
     cy.contains('button', 'Log out').click();
